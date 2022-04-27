37b5fd5d42a146752fdaa39c9feb42cd650fe9fa
ahmadgalea <ahmadgalea@gmail.com>
2022-04-27 15:19:25 +1000

    Added cauldron. Started raycast scripts

---
+++ Assets/Scripts/HousemateData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Housemate")]
+public class HousemateData : ScriptableObject
+{
+    public string name;
+    public string age;
+    public string occupation;
+}
---
+++ Assets/Scripts/ObjectMessagePair.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Object Message Pair")]
+public class ObjectMessagePair : ScriptableObject
+{
+    public GameObject foundObject;
+    public string message;
+}
